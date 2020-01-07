eefcb58172c530310abd40097abb9cf45640b074
Jason Kuhrt <jasonkuhrt@me.com>
2020-01-06 21:50:53 -0500

    fix var in snap

--- tests/system/git.spec.ts
+++ tests/system/git.spec.ts
@@ -18,11 +18,12 @@ describe('streamLog', () => {
     for await (const entry of Git.streamLog({ cwd: ws.dir.path })) {
       entries.push(entry)
     }
+    entries.forEach(e => (e.sha = '__sha__'))
     expect(entries).toMatchInlineSnapshot(`
       Array [
         Object {
           "message": "work 3",
-          "sha": "660a73bc9a4dfc67078c7b5b852a648a81f8e790",
+          "sha": "__sha__",
           "tags": Array [
             "tag-3b",
             "tag-3a",
@@ -30,21 +31,21 @@ describe('streamLog', () => {
         },
         Object {
           "message": "work 2",
-          "sha": "4476e403fe0fb1cfde343907e19c3dc7af3cd701",
+          "sha": "__sha__",
           "tags": Array [
             "tag-2",
           ],
         },
         Object {
           "message": "work 1",
-          "sha": "b79ffbcb9ed2179c0509cd3d1e129c2bc587b50e",
+          "sha": "__sha__",
           "tags": Array [
             "tag-1",
           ],
         },
         Object {
           "message": "initial commit",
-          "sha": "9dce8fcd167b7bbc60b2f370849a0a35655dcc00",
+          "sha": "__sha__",
           "tags": Array [],
         },
       ]
