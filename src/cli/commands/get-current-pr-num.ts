e5796fc793efa0c94c9fa9c005693b24529dde09
Jason Kuhrt <jasonkuhrt@me.com>
2020-02-26 09:27:22 -0500

    exit 1 by default if no pr num found

--- src/cli/commands/get-current-pr-num.ts
+++ src/cli/commands/get-current-pr-num.ts
@@ -1,9 +1,28 @@
-import Command from '@oclif/command'
+import Command, { flags } from '@oclif/command'
 import * as Context from '../../utils/context'
 
 export class GetCurrentPRNum extends Command {
+  static flags = {
+    optional: flags.boolean({
+      description:
+        'Exit 0 if a pr number cannot be found for whatever reason (logical, error, ...)',
+      default: false,
+      char: 'r',
+    }),
+  }
   async run() {
+    const { flags } = this.parse(GetCurrentPRNum)
+
     const context = await Context.scan()
-    this.log(context.currentBranch.pr?.number.toString() ?? '')
+
+    const prNum = context.currentBranch.pr?.number ?? null
+
+    if (prNum !== null) {
+      return process.stdout.write(String(prNum))
+    }
+
+    if (!flags['optional']) {
+      return this.exit(1)
+    }
   }
 }
