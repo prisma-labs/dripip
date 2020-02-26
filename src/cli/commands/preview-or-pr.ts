218d95c45570cac711504636b8b6eacebf33824e
Jason Kuhrt <jasonkuhrt@me.com>
2020-02-26 14:48:53 -0500

    perf: no series calc on preview-or-pr

--- src/cli/commands/preview-or-pr.ts
+++ src/cli/commands/preview-or-pr.ts
@@ -1,11 +1,18 @@
 import Command from '@oclif/command'
-import { getContext } from '../../utils/context'
+import { Octokit } from '@octokit/rest'
+import { createGit } from '../../lib/git'
+import { getLocationContext } from '../../utils/context'
 import { PR } from './pr'
 import { Preview } from './preview'
 
 export class PreviewOrPR extends Command {
   async run() {
-    const context = await getContext()
+    const context = await getLocationContext({
+      git: createGit(),
+      octokit: new Octokit({
+        auth: process.env.GITHUB_TOKEN,
+      }),
+    })
 
     if (context.currentBranch.pr) {
       await PR.run(process.argv.slice(3))
