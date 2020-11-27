9b781f060ebecf429b4c2829ce5c1e7d21c40ef0
Jason Kuhrt <jasonkuhrt@me.com>
2020-11-27 07:30:41 -0500

    tests: flatten describe blocks

--- src/lib/changelog/index.spec.ts
+++ src/lib/changelog/index.spec.ts
@@ -16,42 +16,41 @@ const mockCommits: Release.MockCommit[] = [
   { message: 'feat: blah', version: '0.1.0' },
 ]
 
-describe('.render()', () => {
-  describe('markdown renderer option', () => {
-    const render = (...commits: Release.MockCommit[]) => {
-      return Changelog.Renderers.Markdown.render(Changelog.fromSeries(Release.fromMockCommits(commits)))
-    }
-    it('renders release changelog for the current release series', () => {
-      const changelog = render(...mockCommits)
-      expect(changelog).toMatchSnapshot()
-    })
-    it('shows breaking commits twice, once in breaking section and once in its native section', () => {
-      const changelog = render({ message: 'a: foo\n\nBREAKING CHANGE:\nbar' })
-      expect(changelog).toMatch(/BREAKING CHANGES\n\n.*foo/)
-      expect(changelog).toMatch(/Improvements\n\n.*foo/)
-    })
-    it('shows breaking label if commit breaking except within breaking section', () => {
-      const changelog = render({ message: 'a: foo\n\nBREAKING CHANGE:\nbar' })
-      expect(changelog).toMatch(/Improvements\n\n.*(breaking).*foo/)
-    })
-    it('hides unknown section if no unknown commits', () => {
-      const changelog = render({ message: 'a: b' })
-      expect(changelog).not.toMatch(Changelog.empty().unspecified.label)
-    })
-    it('improvements section prefixes commits with their type', () => {
-      const changelog = render({ message: 'a: foo' })
-      expect(changelog).toMatch(/a: foo/)
-    })
+describe('.render() with markdown', () => {
+  const render = (...commits: Release.MockCommit[]) => {
+    // return Changelog.Renderers.Markdown.render(Changelog.fromSeries(Release.fromMockCommits(commits)))
+    return Changelog.render(Changelog.fromSeries(Release.fromMockCommits(commits)), { as: 'markdown' })
+  }
+  it('renders release changelog for the current release series', () => {
+    const changelog = render(...mockCommits)
+    expect(changelog).toMatchSnapshot()
   })
+  it('shows breaking commits twice, once in breaking section and once in its native section', () => {
+    const changelog = render({ message: 'a: foo\n\nBREAKING CHANGE:\nbar' })
+    expect(changelog).toMatch(/BREAKING CHANGES\n\n.*foo/)
+    expect(changelog).toMatch(/Improvements\n\n.*foo/)
+  })
+  it('shows breaking label if commit breaking except within breaking section', () => {
+    const changelog = render({ message: 'a: foo\n\nBREAKING CHANGE:\nbar' })
+    expect(changelog).toMatch(/Improvements\n\n.*(breaking).*foo/)
+  })
+  it('hides unknown section if no unknown commits', () => {
+    const changelog = render({ message: 'a: b' })
+    expect(changelog).not.toMatch(Changelog.empty().unspecified.label)
+  })
+  it('improvements section prefixes commits with their type', () => {
+    const changelog = render({ message: 'a: foo' })
+    expect(changelog).toMatch(/a: foo/)
+  })
+})
 
-  describe('termimal renderer option', () => {
-    const render = (...commits: Release.MockCommit[]) => {
-      return Changelog.Renderers.Terminal.render(Changelog.fromSeries(Release.fromMockCommits(commits)))
-    }
+describe('.render() with termimal', () => {
+  const render = (...commits: Release.MockCommit[]) => {
+    return Changelog.render(Changelog.fromSeries(Release.fromMockCommits(commits)), { as: 'plain' })
+  }
 
-    it('renders changelog for the current release series', () => {
-      const changelog = stripAnsi(render(...mockCommits))
-      expect(changelog).toMatchSnapshot()
-    })
+  it('renders changelog for the current release series', () => {
+    const changelog = stripAnsi(render(...mockCommits))
+    expect(changelog).toMatchSnapshot()
   })
 })
